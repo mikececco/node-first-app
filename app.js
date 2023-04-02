4b704debb08bd8f268c708da200e21acf3227d6b
Mike Cecconello <mikececco2000@gmail.com>
2023-04-02 21:14:02 +0200

    Redirect page with new dummy file after POST form

--- app.js
+++ app.js
@@ -9,7 +9,24 @@ const http = require('http'); //global module HTTP
 
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'dfdfdfd');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
 
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
