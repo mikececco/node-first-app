13f95120c9ce42c6ac70eebd98af5fbab0f6c377
Mike Cecconello <mikececco2000@gmail.com>
2023-04-10 14:37:00 +0200

    Styled and added public folder - link from app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 // const http = require('http'); //global module HTTP
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,12 +10,13 @@ const shopRoutes = require('./routes/shop')
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>')
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 // const server = http.createServer(app); //connection to routes.js
