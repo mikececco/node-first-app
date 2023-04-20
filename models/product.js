411bdec234fc766e9a03f6b74a6e97cc20210cd6
Mike Cecconello <mikececco2000@gmail.com>
2023-04-20 15:34:28 +0200

    Able to read and store in file

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require('fs')
+const path = require('path')
 
 module.exports = class Product {
     constructor(t) {
@@ -6,7 +7,19 @@ module.exports = class Product {
     }
 
     save() {
+      const p = path.join(
+        path.dirname(process.mainModule.filename), 'data', 'products.json'
+      );
+      fs.readFile(p, (err, fileContent) => {
+        let products = [];
+        if (!err) {
+          products = JSON.parse(fileContent);
+        }
         products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        })
+      });
     }
 
     static fetchAll() {
