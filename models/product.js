56a4dffe77cbaba7d33f120cd5df4f6d12ce2222
Mike Cecconello <mikececco2000@gmail.com>
2023-04-24 18:42:59 +0200

    Added products view and controller

--- models/product.js
+++ models/product.js
@@ -1,5 +1,17 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
+const p = path.join(
+  path.dirname(process.mainModule.filename), 'data', 'products.json'
+);
+
+const getProdutsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  })
+}
 
 module.exports = class Product {
     constructor(t) {
@@ -7,22 +19,15 @@ module.exports = class Product {
     }
 
     save() {
-      const p = path.join(
-        path.dirname(process.mainModule.filename), 'data', 'products.json'
-      );
-      fs.readFile(p, (err, fileContent) => {
-        let products = [];
-        if (!err) {
-          products = JSON.parse(fileContent);
-        }
+      getProdutsFromFile(products => {
         products.push(this);
         fs.writeFile(p, JSON.stringify(products), (err) => {
           console.log(err);
-        })
+        });
       });
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+      getProdutsFromFile(cb)
     }
 }
